d14b360cbe88224ebe1731a313e2ae34a3196d8b
Yiwen Mei <32801294+YiwenMei@users.noreply.github.com>
2019-04-01 19:31:20 -0400

    Add files via upload

--- CastShadow.m
+++ CastShadow.m
@@ -5,23 +5,25 @@
 %% Functionality
 % Calculate the shadow mask of terrain based on the horizontal angle at the
 % direction of solar azimuth and solor altitude (i.e. Hrz > El -> shadowed,
-% Ruiz-Arias et al. 2010).
+% Ruiz-Arias et al. 2010). This function is designed for catersian coordinate
+% only and it determines points that form the loxodrome based on the x and
+% y coordinate. Thus, it is efficient computationally. 
 
 %% Input:
 % X/Y: X/Y-coordinate (in m) of grids of the study domain (N by M, where N &
 %      M refer to the number of grids in lat and lon);
 % Lat: Latitude (in deg) of grids of the study domain (N by M);
 % rs : resolution of grids (m);
-%  Z : orthogonal elevation of grids of the domain (m, N by M);
+%  Z : elevation of grids of the domain (m a.s.l., N by M);
 % Az : Solar azimuth for the study domain (deg, N is 0 clock's wise is +; can
 %      be any dimension representing the Az of the study domain);
 % El : Solar altitude for the study domain (deg, can be any dimension);
 % ndv: no-data value for the inputs dataset (use only one ndv for all inputs);
-% rm : maximum search radius (m, set it to [] to use the location specific rm);
+% rm : maximum search radius (m, unset it to use the location specific rm).
 
 %% Output:
 % Hrz: horizontal angle of terrain at the direction of solar azimuth (deg);
-% SMk: a shadow mask for terrain (0 -> shadowed, 1 -> non-shadowed);
+% SMk: a shadow mask for terrain (0 -> shadowed, 1 -> non-shadowed).
 
 %% Additional note:
 % Please use a Catesian coordinate for the calculation;
@@ -29,47 +31,44 @@
 %  the dimensions are different than Z;
 
 function [hrz,SMk]=CastShadow(X,Y,Lat,rs,Z,Az,El,ndv,rm)
-ins=ceil(rs*sind(45)); % Increment of searching on loxordrome
-
+%% Parameters
 miX=min(X(1,:));
 maX=max(X(1,:));
 miY=min(Y(:,1));
 maY=max(Y(:,1));
 
-k=Z==ndv | El<0;
-X(k)=[];
-X=X';
-Y(k)=[];
-Y=Y';
-
 Az=imresize(Az,size(Z),'bilinear');
-Az(k)=[];
-Az=Az';
 El=imresize(El,size(Z),'bilinear');
-El(k)=[];
-El=El';
+k=Z==ndv | El<0;
+Az=Az(~k);
+El=El(~k);
+X=X(~k);
+Y=Y(~k);
+Lat=Lat(~k);
 
-% Radius of Earth at different latitude (m) 
-r1=6378137; % Earth radius at equator and pole
+% Earth radius changes with latitude
+r1=6378137;
 r2=6356752;
-Lat(k)=[];
-Lat=Lat';
 Ro=sqrt(((r1^2*cosd(Lat)).^2+(r2^2*sind(Lat)).^2)./((r1*cosd(Lat)).^2+(r2*sind(Lat)).^2));
 
-k=find(~k);
+%% Maximum search radius
+k=find(~k); % recycle k
 Zf=Z(k);
 
-%% Maximum search radius
-if isempty(rm)
-  rm=sqrt(Zf.*(2*Ro+Zf));
-  rm(Zf<0)=0;
-else
-  rm=rm*ones(size(Zf));
+switch nargin
+  case {1:7}; error('Not enough number of arguments');
+  case 8
+    rm=sqrt(Zf.*(2*Ro+Zf));
+    rm(Zf<0)=0;
+  case 9
+    rm=rm*ones(size(Zf));
+  otherwise; error('Too many number of arguments');
 end
 N=round(rm/rs);
 clear Lat Ro
 
 %% Horizontal angle on solar azimuth direction
+ins=ceil(rs*sind(45)); % Increment of searching on loxordrome
 Hrz=nan(size(Zf));
 for n=1:max(N)
   hrz=nan(size(Zf));
@@ -107,7 +106,7 @@ for n=1:max(N)
   Hrz=max([Hrz hrz zeros(size(Zf))],[],2,'omitnan');
   clear hrz1 hrz D
 
-% Reduce the maximum search times
+% Reduce the maximum search times using tan(aH_0i)<=tan(aH_0j)=(Z_j-Z_0)/D_j
   tanHrz(isinf(tanHrz) | tanHrz<=0)=NaN;
   rm1=(max(Zf)-Zf(~k1))./tanHrz;
   rmi(~k1)=rm1;
