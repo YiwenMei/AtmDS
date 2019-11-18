088d67d2f01660e39035d6d3eec4432b8c6bc936
Yiwen Mei <32801294+YiwenMei@users.noreply.github.com>
2019-11-17 22:47:01 -0500

    Add files via upload

--- Prec_DS.m
+++ Prec_DS.m
@@ -1,10 +1,10 @@
 % Yiwen Mei (ymei2@gmu.edu)
 % CEIE, George Mason University
-% Last update: 11/06/2018
+% Last update: 9/6/2019
 
 %% Functionality
 % This function evokes the Random Forest algorithm (RF, Breiman, 2001) for the
-% downscaling of precipitation rate using the algorithm developed by Mei et al. 2018.
+% downscaling of precipitation using the algorithm developed by Mei et al. 2019.
 % The function has three different modes - predictor variable selection, model
 % evaluation, and prediction.
 %  1)Predictor Variable Selection Mode is used to determine the predition value
@@ -13,78 +13,93 @@
 %    in impurity due to split of predictors. Based on the results, users can
 %    reduce the model complexity by removing variables with relative low prediction
 %    value.
-%  2)Model Evaluation Mode evokes RF to create classification and regression
-%    tree model for precipitation mask and precipitation rate over "wet pixel".
-%  3)Prediction Mode is used to estimate the binary precipitation mask and values
-%    of precipitation rate.
+%  2)Model Evaluation Mode evokes RF to train classification or regression tree
+%    model for precipitation mask or precipitation rate with the selected predictors
+%    from Selection mode.
+%  3)Prediction Mode is used to estimate the binary precipitation mask or precipitation
+%    rate with the trained model from Evaluation mode.
 
 %% Input
-%  md : mode of the function (can be "Select PermuteError", "Select DecreaseImpure",
-%       "Evaluate mask", "Evaluate rate", "Predict mask" or "Predict rate". Their
-%       meanings are as follow.
-%        - Select PermuteError: use the permuted error as measure of predictor
-%          importance in predictor selection;
-%        - Select DecreaseImpure: use the decrease in impurity due to slit as
-%          predictor importance measure in predictor selection;
-%        - Evaluate mask: evaluate the model for precipitation mask prediction
-%          using Random Forest classification;
-%        - Evaluate rate: evaluate the model for precipitation rate prediction
-%          using Random Forest regreesion;
-%        - Predict mask: predict the precipitation mask using the Random Forest
-%          classification model attained from "Evaluate mask";
-%        - Predict rate: predict the precipitation rate using the Random Forest
-%          regression model attained from "Evaluate rate";
-%  X : (potential) predictors for precipitation mask or precipitation rate;
-%  Y : for the two "Select" modes, this is the precipitation rate; for the two
-%      "Evaluate" mode, this is the precipitation mask or precipitation rate;
-%      for "Predict" mode, this is the Treebagger object stores the RF classification
-%      or regression model attained from the "Evaluate" mode;
-% pr1: in "Select" and "Evaluate", this is the index of the witholding sample
-%      used in model validation; in "Predict rate", this is the residual of precipitation
-%      rate for a time step (set it to "[]" in the case of "Predict mask");
-% pr2: in "Select" and "Evaluate", this is the number of tree grown for the
-%      RF model; in "Predict mask", this is a binary mask indexing the no-data
-%      value of a time step; in "Predict rate", this is a binary mask indexing
-%      the "wet pixel" attained from "Predict mask" for a time step;
-% pr3: in "Select" and "Evaluate", this is the min leaf size parameter (set to
-%      "[]" for "Predict");
-% pr4: in "Select" and "Evaluate", this is a flag to use parallel computing (set
-%      to "UseParallel" for parallel computing and "[]" for not using); set it
-%      to "[]" for "Predict";
-% pth: in the two "Evaluate" modes, this is the full name of file to store the
-%      RF model; in the two "Predict" mode, this is the working directory for
-%      the code (set to "[]" in "Select" modes).
+% md: mode of the function (6 modes) specified as a char array.
+%      - Select PermuteError: use the permuted error as measure of predictor
+%      importance metrics in predictor selection;
+%      - Select DecreaseImpure: save as the previous but uses the decrease in
+%      impurity instead;
+%      - Evaluate mask: train the RF model for precipitation mask prediction
+%       using Random Forest classification;
+%       - Evaluate rate: evaluate the model for precipitation rate prediction
+%       using Random Forest regreesion;
+%       - Predict mask: predict the precipitation mask using the Random Forest
+%       classification model attained from "Evaluate mask";
+%       - Predict rate: predict the precipitation rate using the Random Forest
+%       regression model attained from "Evaluate rate";
+
+% X: for "Select", this is the potential predictors for precipitation rate;
+%    for "Evaluate" and "Predict", this is the selected predictors for precipitation
+%     mask or rate;
+
+% Y: for "Select", this is the precipitation rate;
+%    for "Evaluate", this is the precipitation mask or precipitation rate;
+%    for "Predict" mode, this is the Treebagger object stores the RF classification
+%     or regression model attained from the "Evaluate" mode;
+
+% pr1: for "Select" and "Evaluate", this is the index of the witholding sample,
+%       the day of year, and the location;
+%      for "Predict", this is not requried (for "Predict rate", this can be supplied
+%       as the residual of precipitation rate for a time step if the residual
+%       is added back to the prediction);
+
+% pr2: for "Select" and "Evaluate", this is the number of tree grown for the
+%       RF model;
+%      for "Predict mask", this is a binary mask indexing the no-data value of
+%       a time step;
+%      for "Predict rate", this is a binary mask indexing the "wet pixel" attained
+%       from "Predict mask" for a time step;
+
+% pr3: for "Select" and "Evaluate", this is the minimum leaf size;
+%      for "Predict", this is not required;
+
+% pr4: for "Select" and "Evaluate", this is a flag to use parallel computing
+%       (set to "UseParallel" for parallel computing and "[]" for not using);
+%      for "Predict", this is not required;
+
+% pth: for "Select", this is not requried;
+%      for "Evaluate", this is the full name of file to store the RF model object;
+%      for "Predict", this is the working directory for the code.
 
 %% Output
-% O1: in "Select", O1 has two fields O1.PII and O1.I.
+% O1: for "Select", O1 has two fields O1.PII and O1.I.
 %      - O1.PII stores the predictor importance indices measured by the Predictor
-%        Permuted Delta Error (PPDE) & the Delta Criterion Decision Split (DCDS);
+%      Permuted Delta Error & the Delta Criterion Decision Split;
 %      - O1.I stores the names of predictor sorted by the order of their removal.
-%     in "Evaluate", O1 has two fields O1.cal and O1.val.
-%      - O1.cal stores the model performance metrics (additional note) for the
-%        calibration sample;
-%      - O1.val stores the model performance metrics for the validation sample;
-%     in "Predict", this is the precipitation mask or precipitation rate for
-%     a time step.
-% O2: in "Select", this is the mean Square error (MSE) for the regression tree
-%     model as a function of number of predictor and number of tree;
-%     in "Evaluate", this is the four situations of contingency for classification
-%     and the model residual for regression;
-%     in "Predict", set it to "~".
 
-%% Additional note
-% Model performance metrics:
-%  1)for classification tree model, O2.cal and O2.val stores the number of sample,
-%    rate of missing, correct negative, false alarm, and hit;
-%  2)for regression tree model, O2.cal and O2.val stores the number of sample,
-%    mean relative error, centered root mean square error, correlation coefficient,
-%    Nash Sutcliffe Efficiency and Kling-Gupta Efficiency.,'PredictorSelection','curvature'
+%     for "Evaluate Mask", O1 has two fields O1.EM and O1.ss.
+%      - O1.EM is the miss classification rate as a function of number of tree
+%      for the in-bag, out-of-bag, and validation samples;
+%      - O1.ss is the three sample sizes.
+
+%     for "Evaluate Mask", O1 has three fields O1.EM, O1.ysts and O1.ss.
+%      - O1.EM is the mean squared error (MSE) for the three samples;
+%      - O1.ysts is the mean and variance of the three sample of the response;
+%      - O1.ss is the three sample sizes.
+
+%     for "Predict", this is the precipitation mask or precipitation rate for
+%      a time step.
+
+% O2: for "Select", this is an NT-by-NP-by-3 matrix stores the MSE for the RF
+%      regression models (NT and NP stand for the number of tree and predictor;
+%      the 3 records are for the in-bag, out-of-bag, and validation sample);
+
+%     for "Evaluate", this is the four situations of contingency for classification
+%      and the model residual for regression;
+
+%     for "Predict", this is not required.
 
 function [O1,O2]=Prec_DS(md,X,Y,pr1,pr2,pr3,pr4,pth)
 if strncmp(md,'Select',6)
 %% Variable selection mode
   I=cell(1,size(X,2));
-  Err=nan(size(X,2),pr2,3);
+  EM=nan(size(X,2),pr2,3);
 
 % Model fitting
   CT=cell(size(I));
@@ -114,9 +129,9 @@ if strncmp(md,'Select',6)
     end
 
 % MSE of IB, OoB, and Val sample
-    Err(nt,:,1)=error(Mdl.compact,Mdl.X,Mdl.Y,'weights',Mdl.W,'useifort',~Mdl.OOBIndices);
-    Err(nt,:,2)=oobError(Mdl);
-    Err(nt,:,3)=error(Mdl.compact,X(~pr1,:),Y(~pr1,1),'weights',ones(length(find(~pr1)),1)/length(find(~pr1)));
+    EM(nt,:,1)=error(Mdl.compact,Mdl.X,Mdl.Y,'weights',Mdl.W,'useifort',~Mdl.OOBIndices);
+    EM(nt,:,2)=oobError(Mdl);
+    EM(nt,:,3)=error(Mdl.compact,X(~pr1,:),Y(~pr1,1),'weights',ones(length(find(~pr1)),1)/length(find(~pr1)));
     if nt~=length(I)
       clear Mdl
     end
@@ -141,7 +156,7 @@ if strncmp(md,'Select',6)
 
   O1.PII=PII; % Predictor importance index
   O1.I=I; % Predictor removed per round
-  O2.MSE=Err; % MSE of model
+  O2.EM=EM; % MSE of model
   O2.ysts=ysts; % statistics of y
 
 elseif strncmp(md,'Evaluate',8)
@@ -156,13 +171,14 @@ elseif strncmp(md,'Evaluate',8)
     end
 
 % Miss Classification Rate
-    O1.MSE=nan(pr2,3);
-    O1.MSE(:,1)=error(Mdl.compact,Mdl.X,Mdl.Y,'weights',Mdl.W,'useifort',~Mdl.OOBIndices);
+    O1.EM=nan(pr2,3);
+    O1.EM(:,1)=error(Mdl.compact,Mdl.X,Mdl.Y,'weights',Mdl.W,'useifort',~Mdl.OOBIndices);
     Nib=length(find(~Mdl.OOBIndices))/pr2;
-    O1.MSE(:,2)=oobError(Mdl);
+    O1.EM(:,2)=oobError(Mdl);
     Noob=length(find(Mdl.OOBIndices))/pr2;
-    O1.MSE(:,3)=error(Mdl.compact,X(~pr1,:),Y(~pr1,1),'weights',ones(length(find(~pr1)),1)/length(find(~pr1)));
+    O1.EM(:,3)=error(Mdl.compact,X(~pr1,:),Y(~pr1,1),'weights',ones(length(find(~pr1)),1)/length(find(~pr1)));
     Nval=length(find(~pr1));
+    O1.EM=array2table(O1.EM,'VariableNames',{'MCR_ib','MCR_oob','MCR_val'});
     O1.ss=[Nib Noob Nval];
 
     Mdl=compact(Mdl);
@@ -186,13 +202,14 @@ elseif strncmp(md,'Evaluate',8)
     end
 
 % Mean Squared Error
-    O1.MSE=nan(pr2,3);
-    O1.MSE(:,1)=error(Mdl.compact,Mdl.X,Mdl.Y,'weights',Mdl.W,'useifort',~Mdl.OOBIndices);
+    O1.EM=nan(pr2,3);
+    O1.EM(:,1)=error(Mdl.compact,Mdl.X,Mdl.Y,'weights',Mdl.W,'useifort',~Mdl.OOBIndices);
     Nib=length(find(~Mdl.OOBIndices))/pr2;
-    O1.MSE(:,2)=oobError(Mdl);
+    O1.EM(:,2)=oobError(Mdl);
     Noob=length(find(Mdl.OOBIndices))/pr2;
-    O1.MSE(:,3)=error(Mdl.compact,X(~pr1,:),Y(~pr1,1),'weights',ones(length(find(~pr1)),1)/length(find(~pr1)));
+    O1.EM(:,3)=error(Mdl.compact,X(~pr1,:),Y(~pr1,1),'weights',ones(length(find(~pr1)),1)/length(find(~pr1)));
     Nval=length(find(~pr1));
+    O1.EM=array2table(O1.EM,'VariableNames',{'MSE_ib','MSE_oob','MSE_val'});
     O1.ss=[Nib Noob Nval];
 
     y_ib=[];
